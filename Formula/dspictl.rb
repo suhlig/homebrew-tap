cfeb0628804b80c067304c89086baeec694bbe7d
Steffen Uhlig <steffen@familie-uhlig.net>
2026-05-23 18:12:42 +0200

    Add missing version field

--- Formula/dspictl.rb
+++ Formula/dspictl.rb
@@ -3,6 +3,7 @@ class Dspictl < Formula
   homepage "https://github.com/suhlig/dspi"
   url "https://github.com/suhlig/dspictl/releases/download/v1.0.4/dspictl-v1.0.4-darwin-arm64.tar.gz"
   sha256 "de4204e316080a5cf58de87a13297f18ed4fb4f0891ae73bca35e12d20c72f36"
+  version "1.0.4"
   license "MIT"
 
   depends_on "libusb"
