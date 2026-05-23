7367af7b9099f961ab21dec5d001053e14116e39
Steffen Uhlig <suhlig@users.noreply.github.com>
2026-05-23 21:20:13 +0200

    dspictl 1.1.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/dspictl.rb
+++ Formula/dspictl.rb
@@ -1,9 +1,9 @@
 class Dspictl < Formula
   desc "Control DSPi audio devices from the command line"
   homepage "https://github.com/suhlig/dspi"
-  url "https://github.com/suhlig/dspictl/releases/download/v1.1.0/dspictl-v1.1.0-darwin-arm64.tar.gz"
-  sha256 "481d4e5eeff4abe9ec05e19dbee16f7c57166bfb82576821d6e75b1f1a324f94"
-  version "1.1.0"
+  url "https://github.com/suhlig/dspictl/releases/download/v1.1.1/dspictl-darwin-arm64.tar.gz"
+  sha256 "07386c983721cdc1e46b181553acfad4c4bc9828152701a56bc43ba71c2bd0db"
+  version "1.1.1"
   license "MIT"
 
   depends_on "libusb"
