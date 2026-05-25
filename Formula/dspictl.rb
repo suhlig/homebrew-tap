8666896f2d1d73884cd68f73ea95bf297e699423
Steffen Uhlig <steffen@familie-uhlig.net>
2026-05-25 17:18:41 +0200

    Implement audit suggestions

--- Formula/dspictl.rb
+++ Formula/dspictl.rb
@@ -1,9 +1,9 @@
 class Dspictl < Formula
-  desc "Control DSPi audio devices from the command line"
+  desc "Control DSPi audio devices from the command-line"
   homepage "https://github.com/suhlig/dspi"
   url "https://github.com/suhlig/dspictl/releases/download/v1.1.1/dspictl-darwin-arm64.tar.gz"
-  sha256 "07386c983721cdc1e46b181553acfad4c4bc9828152701a56bc43ba71c2bd0db"
   version "1.1.1"
+  sha256 "07386c983721cdc1e46b181553acfad4c4bc9828152701a56bc43ba71c2bd0db"
   license "MIT"
 
   depends_on "libusb"
@@ -13,6 +13,6 @@ class Dspictl < Formula
   end
 
   test do
-    system "#{bin}/dspictl", "--version"
+    system bin/"dspictl", "--version"
   end
 end
