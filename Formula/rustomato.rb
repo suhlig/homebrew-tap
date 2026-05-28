11c832cffcc1dbc26da648864c1d14d5b130bc61
Steffen Uhlig <steffen@familie-uhlig.net>
2026-05-28 14:20:50 +0200

    Revert multi-arch support for rustomato
    Reconsider if we ever get requests for that

--- Formula/rustomato.rb
+++ Formula/rustomato.rb
@@ -1,18 +1,10 @@
 class Rustomato < Formula
   desc "A simple Pomodoro timer for the command line"
   homepage "https://github.com/suhlig/rustomato"
+  url "https://github.com/suhlig/rustomato/releases/download/v0.0.12/rustomato-darwin-arm64.tar.gz"
+  sha256 "46ed1261765bbca74540099147c53723cbcd1a99a204a98ba7feebd7627a9fed"
   license "MIT"
   version "0.0.12"
-
-  on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/suhlig/rustomato/releases/download/v0.0.12/rustomato-darwin-amd64.tar.gz"
-      sha256 "84c4650446ea0eb48d3ce1ae55d1537e2fad6d8ea6131af3441a4ef282c93f3d"
-    else
-      url "https://github.com/suhlig/rustomato/releases/download/v0.0.12/rustomato-darwin-arm64.tar.gz"
-      sha256 "46ed1261765bbca74540099147c53723cbcd1a99a204a98ba7feebd7627a9fed"
-    end
-  end
 
   def install
     bin.install "rustomato"
