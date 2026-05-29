9734103d073c4d1a4a71e26778691ada008acacb
Steffen Uhlig <suhlig@users.noreply.github.com>
2026-05-29 23:09:49 +0200

    rustomato 1.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/rustomato.rb
+++ Formula/rustomato.rb
@@ -1,10 +1,10 @@
 class Rustomato < Formula
   desc "A simple Pomodoro timer for the command line"
   homepage "https://github.com/suhlig/rustomato"
-  url "https://github.com/suhlig/rustomato/releases/download/v1.1.0/rustomato-darwin-arm64.tar.gz"
-  sha256 "797a6c149c6087f5d1a10186ffa0d73d0e96f2f21c71d8ca1d9cd7326e39a571"
+  url "https://github.com/suhlig/rustomato/releases/download/v1.2.0/rustomato-darwin-arm64.tar.gz"
+  sha256 "65de392cf6dbc8acb91871c273a04aa50b01af79596396f784f60faf351f6cb8"
   license "MIT"
-  version "1.1.0"
+  version "1.2.0"
 
   def install
     bin.install "rustomato"
